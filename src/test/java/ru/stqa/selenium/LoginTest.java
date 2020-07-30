cab492d3f3181bbaa8f4b77f653b0190f4bf55fa
Yaremenko Roman <roman.jaremenko@gmail.com>
2020-07-30 23:22:03 +0300

    LoginTest added

---
+++ src/test/java/ru/stqa/selenium/LoginTest.java
@@ -0,0 +1,32 @@
+package ru.stqa.selenium;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LoginTest {
+
+  private WebDriver driver;
+
+  @Before
+  public void start() {
+    driver = new ChromeDriver();
+  }
+
+  @Test
+  public void myFirstTest() {
+    driver.get("http://localhost/litecart/admin/");
+    driver.findElement(By.name("username")).sendKeys("admin");
+    driver.findElement(By.name("password")).sendKeys("admin");
+    driver.findElement(By.name("login")).click();
+  }
+
+  @After
+  public void stop() {
+    driver.quit();
+    driver = null;
+  }
+}
